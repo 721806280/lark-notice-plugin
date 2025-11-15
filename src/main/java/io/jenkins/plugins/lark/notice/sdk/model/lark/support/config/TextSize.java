78c4012ec74586e59d7187ecf34d16c2c33da0c0
721806280 <721806280@qq.com>
2025-11-15 16:06:38 +0800

    feat: map 'default' JSON property in TextSize

--- src/main/java/io/jenkins/plugins/lark/notice/sdk/model/lark/support/config/TextSize.java
+++ src/main/java/io/jenkins/plugins/lark/notice/sdk/model/lark/support/config/TextSize.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins.lark.notice.sdk.model.lark.support.config;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -22,5 +23,6 @@ public class TextSize {
     /**
      * 在无法差异化配置字号的旧版飞书客户端上，生效的字号属性。选填
      */
+    @JsonProperty("default")
     private String def;
 }
